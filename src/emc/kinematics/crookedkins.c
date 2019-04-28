839ef9964803d8f10fedb9eda44e727d3a3f853b
Totalschaden1997 <david.reiser1@web.de>
2019-04-28 15:11:07 +0200

    Created crookedkins.c file and header

---
+++ src/emc/kinematics/crookedkins.c
@@ -0,0 +1,15 @@
+/********************************************************************
+* Description: tripodkins.c
+*   Kinematics for crooked 3 axis machines
+*
+*   Derived from a work by Fred Proctor
+*
+* Author: David Reiser
+* License: GPL Version 2
+* System: Linux
+*    
+* Copyright (c) 2019 All rights reserved.
+*
+* Last change:28th April 2019
+********************************************************************/
+
