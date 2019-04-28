001f44b413e147bdc20ab7d1134edb5f94b05e80
Totalschaden1997 <david.reiser1@web.de>
2019-04-28 16:02:59 +0200

    extended header and defined hal pins

--- src/emc/kinematics/crookedkins.c
+++ src/emc/kinematics/crookedkins.c
@@ -1,8 +1,10 @@
 /********************************************************************
-* Description: tripodkins.c
+* Description: crookedkins.c
 *   Kinematics for crooked 3 axis machines
+*   Designed to compensate angular alignment errors between the axes and spindle
+*   Assuming the spindle is aligned normal to the XY-plane
 *
-*   Derived from a work by Fred Proctor
+*   Derived from a work by Fred Proctor, Alex Joni & Chris Radek
 *
 * Author: David Reiser
 * License: GPL Version 2
@@ -13,3 +15,22 @@
 * Last change:28th April 2019
 ********************************************************************/
 
+#include "kinematics.h"             /* these decls */
+#include "rtapi_math.h"             /*realtime math library*/
+
+#include "hal.h"
+struct haldata {
+  hal_float *
+} *haldata = 0;
+
+#define XYang (*(haldata->xyang))   /*angle between X and Y*/
+#define XZang (*(haldata->xyang))   /*angle between X and Z*/
+#define YZang (*(haldata->xyang))   /*angle between Y and Z*/
+
+int kinematicsForward(const double *joints,
+		                  EmcPose * pos,
+		                  const KINEMATICS_FORWARD_FLAGS * fflags,
+                      KINEMATICS_INVERSE_FLAGS * iflags)
+{
+  
+}
